c064b24e7210dc83e94892fdfdb3ce9662f5ffea
oldiachenko <59308469+oldiachenko@users.noreply.github.com>
2020-03-24 12:33:43 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { moviesData } from "../moviesData";
+// import { moviesData } from "../moviesData";
 import MovieItem from "./MovieItem";
+import { API_URL, API_KEY_3 } from "../utils/api";
+import MovieTabs from "./MovieTabs";
 
 // UI = fn(state, props)
 
@@ -11,9 +13,26 @@ class App extends React.Component {
     super();
 
     this.state = {
-      movies: moviesData,
-      moviesWillWatch: []
+      movies: [],
+      moviesWillWatch: [],
+      sort_by: "popularity.desc"
     };
+  }
+
+  componentDidMount() {
+    fetch(
+      `${API_URL}/discover/movie?api_key=${API_KEY_3}&sort_by=${
+        this.state.sort_by
+      }`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          movies: data.results
+        });
+      });
   }
 
   deleteMovie = movie => {
@@ -47,11 +66,16 @@ class App extends React.Component {
   };
 
   render() {
-    console.log("render", this);
     return (
       <div className="container">
         <div className="row mt-4">
           <div className="col-9">
+            <div className="row mb-4">
+              <div className="col-12">
+                <MovieTabs />
+              </div>
+            </div>
+
             <div className="row">
               {this.state.movies.map(movie => {
                 return (
---
+++ src/components/MovieTabs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MovieTabs = () => {
+  return (
+    <ul className="tabs nav nav-pills">
+      <li className="nav-item">
+        <div className="nav-link active">Popularity desc</div>
+      </li>
+      <li className="nav-item">
+        <div className="nav-link">Revenue desc</div>
+      </li>
+      <li className="nav-item">
+        <div className="nav-link">Vote average desc</div>
+      </li>
+    </ul>
+  );
+};
+
+export default MovieTabs;
